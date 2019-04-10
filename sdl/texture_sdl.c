bc50ca8d42040fefe2ed3125d1af7d3826c3fd0d
wchen <406653843@qq.com>
2019-04-10 08:34:46 +0800

    Update sdl

--- sdl/texture_sdl.c
+++ sdl/texture_sdl.c
@@ -5,9 +5,16 @@ int main(int argv, char *argc[])
 {
 	int quit = 1;
 	SDL_Event event;
+
+	SDL_Texture *texture = NULL;
+	SDL_Rect rect;
+	rect.w = 30;
+	rect.h = 30;
+
 	SDL_Window *window = NULL;
 	SDL_Renderer *render = NULL;
 	SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
+	SDL_Log("Hello world!");
 
 	window = SDL_CreateWindow("SDL2 Window",
 			200,
@@ -27,16 +34,18 @@ int main(int argv, char *argc[])
                 goto __DESTROY_WINDOW;
         }
 
+	/*
 	SDL_RenderClear(render);
 	SDL_SetRenderDrawColor(render, 255, 0, 0, 255);
 	SDL_RenderPresent(render);
 	//SDL_Delay(30000);
+	*/
 
-	SDL_Texture *texture = SDL_CreateTexture(render,
-			SDL_PIXELFORMAT_RGBA8888,
-			SDL_TEXTUREACCESS_TARGET,
-			600,
-			450);
+	texture = SDL_CreateTexture(render,
+		SDL_PIXELFORMAT_RGBA8888,
+		SDL_TEXTUREACCESS_TARGET,
+		600,
+		480);
 
 	if (!texture) {
 		SDL_Log("Failed to Create Texture!");
@@ -44,7 +53,7 @@ int main(int argv, char *argc[])
 	}
 
 	do {
-		SDL_WaitEvent(&event);
+		SDL_PollEvent(&event);
 		switch (event.type) {
 			case SDL_QUIT:
 				quit = 0;
@@ -54,7 +63,21 @@ int main(int argv, char *argc[])
 				break;
 		}
 
+		rect.x = rand() % 600;
+		rect.y = rand() % 450;
 
+		SDL_SetRenderTarget(render, texture);
+		SDL_SetRenderDrawColor(render, 0, 0, 0, 0);
+		SDL_RenderClear(render);
+
+		SDL_RenderDrawRect(render, &rect);
+		SDL_SetRenderDrawColor(render, 255, 0, 0, 0);
+		SDL_RenderFillRect(render, &rect);
+
+		SDL_SetRenderTarget(render, NULL);
+		SDL_RenderCopy(render, texture, NULL, NULL);
+
+		SDL_RenderPresent(render);
 
 	} while (quit);
 
