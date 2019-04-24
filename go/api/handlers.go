f11627809d475ee494c8aab512109881387cb504
wchen <406653843@qq.com>
2019-04-24 08:33:08 +0800

    update go

--- go/api/handlers.go
+++ go/api/handlers.go
@@ -7,10 +7,11 @@ import (
 )
 
 func CreateUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	io.WriterString(w, "Create User handler")
+	io.WriteString(w, "Create User handler")
 }
 
 func Login(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	uname := p.ByName("user_name")
-	io.WriterString(w, uname)
+	io.WriteString(w, uname)
 }
+
---
+++ go/api/response.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+func sendErrorResponse(w http.ResponseWriter) {}
+
+func sendNormalResponse(w http.ResponseWriter) {}
+
