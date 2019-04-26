ec7311eef377829a4b1434d6f41a66d2c1242752
wchen <406653843@qq.com>
2019-04-26 08:53:06 +0800

    update go

--- go/api/dbops/api.go
+++ go/api/dbops/api.go
@@ -2,14 +2,23 @@ package dbops
 
 import (
 	"database/sql"
+	"github.com/go-driver/mysql"
 )
 
 func openConn() *sql.DB {
+	dbConn, err := sql.Open("mysql", "root:wangchen18606632@#@tcp(localhost:3306)/video_server?charset=utf8")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return dbConn
 }
 
 func AddUserCredentital(loginName string, pwd string) error {
+	db := openConn()
 }
 
 func GetUserCredential(loginName string) (string, error) {
+	db := openConn()
 }
 
