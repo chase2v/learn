7b1cab27ec28454d7b6ad3ece82329b748688492
wchen <406653843@qq.com>
2019-05-05 19:19:22 +0800

    update go

--- go/api/dbops/api_test.go
+++ go/api/dbops/api_test.go
@@ -2,6 +2,8 @@ package dbops
 
 import (
 	"testing"
+	"strconv",
+	"time",
 )
 
 func init(dblogin, truncate tables)
@@ -57,4 +59,36 @@ func testRegetUser(t *testing.T) {
 		t.Errorf("Deleting user test failed")
 	}
 }
+
+func TestComments(t *testing.T) {
+	claerTables()
+	t.Run("AddUser", testAddUser)
+	t.Run("AddComments", testAddComments)
+	t.Run("ListComments", testListComments)
+}
+
+func testAddComments( * testing.T) {
+	vid := "12345"
+	aid := 1
+	content := "I like this video"
+
+	err := AddNewComments(vid, aid, content)
+
+	if err != nil {
+		t.Errorf("Error of AddComments: %v", err)
+	}
+}
+
+func testListComments(t *testing.T) {
+	vid ::= "12345"
+	from := 1514764800
+	to, _ := strconv.Atoi(strconv.FromInt(time.Now().UnixNano()/1000000000, 10))
+	res, err := ListComments(vid, from, to)
+	if err != nil {
+		t.Errorf("Error of ListComments: %v", err)
+	}
+
+	for i, ele := range res {
+		fmt.Printf("comment: %d, %v \n", i, ele)
+	}
 
